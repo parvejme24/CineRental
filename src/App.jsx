3f7fec273fd165b46432e1d7d03855fb67abd836
Md Parvej <mdparvejme24@gmail.com>
2024-01-25 00:30:31 +0600

    display all movies card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import MovieList from "./cine/MovieList";
 
 export default function App() {
   return (
@@ -9,7 +10,7 @@ export default function App() {
       <main>
         <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
           <Sidebar />
-
+          <MovieList />
         </div>
       </main>
 
--- src/cine/MovieCard.jsx
+++ src/cine/MovieCard.jsx
@@ -1,11 +1,12 @@
-export default function MovieCard() {
+export default function MovieCard({ movie }) {
+    const { cover, title, descriptio, gener, rating, price } = movie
     return (
         <>
             <figure class="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
                 <img class="w-full object-cover" src="./assets/movie-1.png" alt="" />
                 <figcaption class="pt-4">
-                    <h3 class="text-xl mb-1">Iron Man</h3>
-                    <p class="text-[#575A6E] text-sm mb-2">Action/Adventure/Sci-fi</p>
+                    <h3 class="text-xl mb-1">{title}</h3>
+                    <p class="text-[#575A6E] text-sm mb-2">{descriptio}</p>
                     <div class="flex items-center space-x-1 mb-5">
                         <img src="./assets/star.svg" width="14" height="14" alt="" />
                         <img src="./assets/star.svg" width="14" height="14" alt="" />
@@ -16,7 +17,7 @@ export default function MovieCard() {
                     <a class="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
                         href="#">
                         <img src="./assets/tag.svg" alt="" />
-                        <span>$100 | Add to Cart</span>
+                        <span>${price} | Add to Cart</span>
                     </a>
                 </figcaption>
             </figure>
